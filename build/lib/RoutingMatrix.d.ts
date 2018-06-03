17be8e42e71c373096397788fb339eaeb68e8930
David Lee <david.isaac.lee@gmail.com>
2018-06-03 10:19:08 -0700

    Delete build folder from repo

--- build/lib/RoutingMatrix.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-declare type Props = React.HTMLAttributes<HTMLDivElement>;
-export declare class RoutingMatrix extends React.Component<Props, {}> {
-    render(): React.DetailedReactHTMLElement<Readonly<{
-        children?: React.ReactNode;
-    }> & Readonly<React.HTMLAttributes<HTMLDivElement>>, HTMLElement>;
-}
-export {};
